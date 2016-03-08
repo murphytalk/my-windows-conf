ffd9305740c450803fb0e57081ffabe874d61aac
Mu Lu <murphytalk@gmail.com>
2016-03-08 23:43:20 +0900

    batch script to make symbolic links

---
+++ mklink.bat
@@ -0,0 +1,22 @@
+@echo off
+set MSYS=c:\msys64
+set MSYS2CONF=%CD%
+
+cd %MSYS%\usr\bin
+echo dir1 %CD%
+rem mklink /J vi vim.exe
+
+cd %MSYS%\etc\profile.d
+echo dir2 %CD%
+
+rem cd %MSYS2CONF%\profile.d
+rem echo dir3 %CD%
+
+set D=%MSYS2CONF%\profile.d
+for %%f in (%D%\*) do (
+    rem use for/? to see explanation of all modifiers
+    mklink /J %%~nxf %D%\%%~nxf 
+)
+
+
+cd %MSYS2CONF%
