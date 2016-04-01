21e412f21a3183588fbccb72e7e099b09c70eef6
Mu Lu <murphytalk@gmail.com>
2016-04-01 12:59:13 +0900

    more scripts

--- mklink.bat
+++ mklink.bat
@@ -4,7 +4,7 @@ set MSYS2CONF=%CD%
 
 cd %MSYS%\usr\bin
 echo dir1 %CD%
-rem mklink /J vi vim.exe
+rem mklink vi vim.exe
 
 cd %MSYS%\etc\profile.d
 echo dir2 %CD%
@@ -15,7 +15,7 @@ rem echo dir3 %CD%
 set D=%MSYS2CONF%\profile.d
 for %%f in (%D%\*) do (
     rem use for/? to see explanation of all modifiers
-    mklink /J %%~nxf %D%\%%~nxf 
+    mklink %%~nxf %D%\%%~nxf 
 )
 
 
---
+++ msys2.cmd
@@ -0,0 +1,4 @@
+@echo off
+set HOME=/c/Users/%USERNAME%
+set GIT_SSH=/c/Apps/plink.exe
+start c:\msys64\mingw64_shell.bat
